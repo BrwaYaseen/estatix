2e954008d52fd6edecb2bd2b3aa9469eec2755c9
Brwa Yaseen <brwayasin29@gmail.com>
2024-08-15 15:51:56 +0300

    updating schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,19 @@
 import { integer, pgEnum, pgTable, text, varchar } from "drizzle-orm/pg-core";
 
 export const roleEnum = pgEnum("role", ["admin", "seller", "user"]);
+
 export const usersTable = pgTable("users", {
   id: varchar("id").primaryKey(),
   name: text("name").notNull(),
   role: roleEnum("role").default("user").notNull(),
 });
+
+export const cityEnum = pgEnum("city", [
+  "erbil",
+  "sulaymaniyah",
+  "duhok",
+  "halabja",
+]);
 
 export const propertyTable = pgTable("property", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity({ startWith: 1000 }),
@@ -14,4 +22,8 @@ export const propertyTable = pgTable("property", {
   area: integer("area").notNull(),
   bed: integer("bed").notNull(),
   bath: integer("bath").notNull(),
+  city: cityEnum("city").notNull().default("erbil"),
+  userId: varchar("user_id")
+    .references(() => usersTable.id)
+    .notNull(),
 });
