b12ac32394443038b6a26e45e88c6a6dc0034d45
Brwa Yaseen <brwayasin29@gmail.com>
2024-08-15 15:37:40 +0300

    updating  schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,11 @@
-import { integer, pgTable, text } from "drizzle-orm/pg-core";
+import { integer, pgEnum, pgTable, text, varchar } from "drizzle-orm/pg-core";
+
+export const roleEnum = pgEnum("role", ["admin", "seller", "user"]);
+export const usersTable = pgTable("users", {
+  id: varchar("id").primaryKey(),
+  name: text("name").notNull(),
+  role: roleEnum("role").default("user").notNull(),
+});
 
 export const propertyTable = pgTable("property", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity({ startWith: 1000 }),
